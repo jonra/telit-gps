4dcdfe4ed13ba54713958ba819fc611217f33e7b
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-17 23:29:25 +0100

    Added output redirecting to telit.log file

--- setup_environment.sh
+++ setup_environment.sh
@@ -9,6 +9,6 @@
 	printf '%s\n' 'Setting up cron job'
 	DIR="$(dirname "$(realpath "$0")")"
 	crontab -l > crontab_telit
-	printf '%s\n' "@reboot ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.sh && /usr/bin/python3 ${DIR}/GNSS_to_MQTT.py" >> crontab_telit
+	printf '%s\n' "@reboot ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.sh >> ${HOME}/telit.log 2>&1 && /usr/bin/python3 ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1" >> crontab_telit
 	crontab crontab_telit
 	rm crontab_telit
