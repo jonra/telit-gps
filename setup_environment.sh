8103a59ef090e6769832c1b7679fceb3a81e5873
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-17 23:01:04 +0100

    Changed systemd to cron job

--- setup_environment.sh
+++ setup_environment.sh
@@ -6,7 +6,9 @@
 	printf '%s\n' 'Installing required Python3 libraries'
 	pip3 install pyserial paho-mqtt
 	
-	printf '%s\n' 'Setting up telit-gps service'
-	sudo cp ./telit-gps.service /etc/systemd/system
-	sudo systemctl daemon-reload
-	sudo systemctl enable telit-gps.service
+	printf '%s\n' 'Setting up cron job'
+	TELIT-GPS_DIR="$(dirname "$(realpath "$0")")"
+	crontab -l > crontab_telit
+	printf '%s\n' "@reboot ${TELIT-GPS_DIR}/clear_log.sh && ${TELIT-GPS_DIR}/start_gnss_stream.sh && /usr/bin/python3 ${TELIT-GPS_DIR}/GNSS_to_MQTT.py" >> crontab_telit
+	crontab crontab_telit
+	rm crontab_telit
