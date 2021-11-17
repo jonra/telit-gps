debb4d8bc0dc8a50e253c89652683957caf91045
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-17 23:17:30 +0100

    Renamed TELIT-GPS_DIR to DIR

--- setup_environment.sh
+++ setup_environment.sh
@@ -7,8 +7,8 @@
 	pip3 install pyserial paho-mqtt
 
 	printf '%s\n' 'Setting up cron job'
-	TELIT-GPS_DIR="$(dirname "$(realpath "$0")")"
+	DIR="$(dirname "$(realpath "$0")")"
 	crontab -l > crontab_telit
-	printf '%s\n' "@reboot ${TELIT-GPS_DIR}/clear_log.sh && ${TELIT-GPS_DIR}/start_gnss_stream.sh && /usr/bin/python3 ${TELIT-GPS_DIR}/GNSS_to_MQTT.py" >> crontab_telit
+	printf '%s\n' "@reboot ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.sh && /usr/bin/python3 ${DIR}/GNSS_to_MQTT.py" >> crontab_telit
 	crontab crontab_telit
 	rm crontab_telit
