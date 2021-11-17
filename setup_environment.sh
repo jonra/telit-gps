edb23b96e4cda0304aee067fad26854aa3e3c5a6
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-17 21:44:24 +0100

    Added telit-gps service setup

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 	printf '%s\n' 'Installing required packages'
-	sudo apt -y install minicom python3-pip
+	apt -y install minicom python3-pip git
 	
 	printf '%s\n' 'Installing required Python3 libraries'
 	pip3 install pyserial paho-mqtt
+	
+	printf '%s\n' 'Setting up telit-gps service'
+	sudo cp ./telit-gps.service /etc/systemd/system
+	sudo systemctl daemon-reload
+	sudo systemctl enable telit-gps.service
