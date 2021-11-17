1f069063ee014d3cdca2994465cd108ebdd431a8
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-17 21:48:41 +0100

    Changed apt to sudo apt

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 	printf '%s\n' 'Installing required packages'
-	apt -y install minicom python3-pip git
+	sudo apt -y install minicom python3-pip git
 	
 	printf '%s\n' 'Installing required Python3 libraries'
 	pip3 install pyserial paho-mqtt
