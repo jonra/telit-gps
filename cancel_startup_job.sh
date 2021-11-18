7570b10034fac01ee4452f52db07b397d9351843
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-18 03:37:31 +0100

    Added GNSS_to_MQTT.py process kill

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -11,5 +11,5 @@
 	rm crontab_current
 	
 	printf 's\n\n' 'Stopping GNSS_to_MQTT.py'
-	kill $(ps -ef | grep 'GNSS_to_MQTT.py' | grep -v grep | awk '/[G]NSS_to_MQTT.py/{print $2}')
+	kill $(ps -ef | grep '[G]NSS_to_MQTT.py' | awk '{print $2}')
 	
