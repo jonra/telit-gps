b7805f44eb3fa9b3c735a68c094340de48c8ad48
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-18 02:51:04 +0100

    Removed start_gnss_stream.sh script output redirecting to telit.log

--- set_up_startup_job.sh
+++ set_up_startup_job.sh
@@ -3,6 +3,6 @@
 	printf '%s\n\n' 'Setting up the cron job for script automation'
 	DIR="$(dirname "$(realpath "$0")")"
 	crontab -l > crontab_telit
-	printf '%s\n' "@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.sh >> 2>&1 ${HOME}/telit.log && /usr/bin/python3 ${DIR}/GNSS_to_MQTT.py >> 2>&1 ${HOME}/telit.log" >> crontab_telit
+	printf '%s\n' "@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.sh ${HOME}/telit.log && /usr/bin/python3 ${DIR}/GNSS_to_MQTT.py >> 2>&1 ${HOME}/telit.log" >> crontab_telit
 	crontab crontab_telit
 	rm crontab_telit
