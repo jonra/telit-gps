4640c743d4d7bd3038a38ea1d3aaa43bf80d6353
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-18 03:27:49 +0100

    Added GNSS_to_MQTT.py process kill

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-	printf '%s\n\n' 'Cancelling the cron job for script automation'
+	printf '%s\n' 'Cancelling the cron job for script automation'
 	. ./stop_gnss_stream.sh >> ${HOME}/telit.log 2>&1
-	ps -ef | grep 'GNSS_to_MQTT.py' | grep -v grep | awk '/[G]NSS_to_MQTT.py/{print $2}' | xargs kill
 	crontab -l > crontab_current
 	grep -v 'start_gnss_stream.sh' crontab_current > tmpfile
 	mv tmpfile crontab_current
 	crontab crontab_current
 	rm crontab_current
+	
+	printf 's\n\n' 'Stopping GNSS_to_MQTT.py'
+	ps -ef | grep 'GNSS_to_MQTT.py' | grep -v grep | awk '/[G]NSS_to_MQTT.py/{print $2}' | xargs kill
