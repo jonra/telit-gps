72d3246cdb2241dc1c1cdb9ad549ed9a34f69146
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-18 13:44:06 +0100

    Modified to reflect the Python shebang change

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -5,7 +5,7 @@
 	
 	printf '%s\n' 'Cancelling the cron job for script automation'
 	crontab -l > crontab_current
-	grep -v 'start_gnss_stream.sh' crontab_current > tmpfile
+	grep -v 'start_gnss_stream.py' crontab_current > tmpfile
 	mv tmpfile crontab_current
 	crontab crontab_current
 	rm crontab_current
--- set_up_startup_job.sh
+++ set_up_startup_job.sh
@@ -3,6 +3,6 @@
 	printf '%s\n\n' 'Setting up the cron job for script automation'
 	DIR="$(dirname "$(realpath "$0")")"
 	crontab -l > crontab_telit
-	printf '%s\n' "@reboot sleep 10 && ${DIR}/clear_log.sh && /usr/bin/python3 ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && /usr/bin/python3 ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1" >> crontab_telit
+	printf '%s\n' "@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1" >> crontab_telit
 	crontab crontab_telit
 	rm crontab_telit
