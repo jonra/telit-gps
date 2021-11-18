8fcffda4244130f065bb01a4755fe4c91c4c3db6
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-18 03:00:26 +0100

    Fixed some errors

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-	. ./stop_gnss_stream.sh
-
 	printf '%s\n\n' 'Cancelling the cron job for script automation'
+	. ./stop_gnss_stream.sh >> ${HOME}/telit.log 2>&1
 	crontab -l > crontab_current
 	grep -v 'start_gnss_stream.sh' crontab_current > tmpfile
 	mv tmpfile crontab_current
