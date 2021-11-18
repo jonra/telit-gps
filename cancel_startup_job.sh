3cf5cd332a6d3d6fcfcef96d386ee4706da05a48
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-18 01:50:27 +0100

    Moved tmpfile renaming to the next line

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -2,6 +2,7 @@
 
 	printf '%s\n\n' 'Cancelling the cron job for script automation'
 	crontab -l > crontab_current
-	grep -v 'start_gnss_stream.sh' crontab_current > tmpfile && mv tmpfile crontab_current
+	grep -v 'start_gnss_stream.sh' crontab_current > tmpfile
+	mv tmpfile crontab_current
 	crontab crontab_current
 	rm crontab_current
