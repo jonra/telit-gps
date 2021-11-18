c08cd121b325034863bbe8549c64a35d4a3c57b5
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-18 13:51:31 +0100

    Changed order of operations

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+	printf '%s\n\n' 'Stopping GNSS_to_MQTT.py'
+	kill $(ps -ef | grep '[G]NSS_to_MQTT.py' | awk '{print $2}') 2> /dev/null
+
 	printf '%s\n' 'Initiating the script that stops the GNSS stream'
 	/usr/bin/python3 ./stop_gnss_stream.py >> ${HOME}/telit.log 2>&1
-	
+
 	printf '%s\n' 'Cancelling the cron job for script automation'
 	crontab -l > crontab_current
 	grep -v 'start_gnss_stream.py' crontab_current > tmpfile
 	mv tmpfile crontab_current
 	crontab crontab_current
 	rm crontab_current
-	
-	printf '%s\n\n' 'Stopping GNSS_to_MQTT.py'
-	kill $(ps -ef | grep '[G]NSS_to_MQTT.py' | awk '{print $2}') 2> /dev/null
-	
