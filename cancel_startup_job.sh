a0ea0cae7cb458ed82208d9b3c625d57715d9cb2
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-21 17:14:32 +0100

    Fixed

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -8,6 +8,7 @@
 
 	printf '%s\n' 'Cancelling the cron job for script automation'
 	crontab -l > crontab_current
+	DIR="$(dirname "$(realpath "$0")")"
 	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1"
 	printf '%s\n' "$cronjob"
 	if grep -q "$cronjob" crontab_current; then
