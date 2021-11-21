24f5a0f1d87147a88ef06fbf5b13f6ac963b1531
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-21 23:46:27 +0100

    Script to set up SIM data over QMI interface

---
+++ set_up_SIM_data_over_QMI.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+	printf '%s\n' 'Setting up the SIM data connection over QMI interface'
+	sleep 1
+	
+	printf '%s\n' 'Checking the compatibility of the module'
+	i=0
+	while [ \( lsusb -t | ! grep -q 'qmi-wwan' \) -a \( lsusb -t | ! grep -q 'GobiNet' \) -a \( $i -lt 5 \)]
+	do
+		i=$(expr $i+1)
+		printf '\r%s\r' 'AT#USBCFG=0' > /dev/ttyUSB3
+		sleep 10
+		printf '\r%s\r' 'AT#REBOOT' > /dev/ttyUSB3
+		sleep 5
+	done
+	if [ $i -ge 5 ]; then
+		printf '%s\n\n' "Module doesn't support QMI interface, terminating"
+		exit 1
+	fi
+	sleep 1
+	
+	printf '%s\n' 'Making sure the module is ready'
+	if sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode | ! grep -q 'online'; do
+		sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'
+	fi
+	sleep 1
+	
+	printf '%s\n' 'Configuring the network interface for the raw-ip protocol'
+	sudo ip link set wwan0 down
+	printf '%c' 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip
+	sudo ip link set wwan0 up
+	sleep 1
+	
+	printf '%s\n' 'Connecting to the mobile network'
+	# Change the apn, username and password according to the information provided by your SIM operator
+	sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='internet',username='',password='',ip-type=4" --client-no-release-cid
+	sleep 1
+	
+	printf '%s\n' 'Configuring the IP address and the default route'
+	sudo udhcpc -q -f -i wwan0
+	printf '\n'
