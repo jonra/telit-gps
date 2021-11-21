6b28b722abe097d188d9ce05c4819edd73f70eeb
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-21 16:42:06 +0100

    cronjob call fix

--- set_up_startup_job.sh
+++ set_up_startup_job.sh
@@ -4,8 +4,8 @@
 	DIR="$(dirname "$(realpath "$0")")"
 	crontab -l > crontab_telit
 	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1"
-	if ! grep -q $cronjob crontab_telit; then
-		printf '%s\n' $cronjob >> crontab_telit
+	if ! grep -q "$cronjob" crontab_telit; then
+		printf '%s\n' "$cronjob" >> crontab_telit
 		crontab crontab_telit
 		printf '\n'
 	else
