a23b23a1465dd17e8f7709ef49a909807a7c1db2
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-21 16:56:50 +0100

    Upgraded

--- cancel_startup_job.sh
+++ cancel_startup_job.sh
@@ -4,11 +4,17 @@
 	kill $(ps -ef | grep '[G]NSS_to_MQTT.py' | awk '{print $2}') 2> /dev/null
 
 	printf '%s\n' 'Initiating the script that stops the GNSS stream'
-	./stop_gnss_stream.py >> ${HOME}/telit.log 2>&1
+	./stop_gnss_stream.py >> "${HOME}/telit.log" 2>&1
 
-	printf '%s\n\n' 'Cancelling the cron job for script automation'
+	printf '%s\n' 'Cancelling the cron job for script automation'
 	crontab -l > crontab_current
-	grep -v 'start_gnss_stream.py' crontab_current > tmpfile
-	mv tmpfile crontab_current
-	crontab crontab_current
+	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1"
+	if grep -q "$cronjob" crontab_current; then
+		grep -v "$cronjob" crontab_current > tmpfile
+		mv tmpfile crontab_current
+		crontab crontab_current
+		printf '\n'
+	else
+		printf '%s\n\n' 'Cron job was not found'
+	fi
 	rm crontab_current
--- set_up_startup_job.sh
+++ set_up_startup_job.sh
@@ -2,13 +2,13 @@
 
 	printf '%s\n' 'Setting up the cron job for script automation'
 	DIR="$(dirname "$(realpath "$0")")"
-	crontab -l > crontab_telit
+	crontab -l > crontab_current
 	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1"
-	if ! grep -q "$cronjob" crontab_telit; then
-		printf '%s\n' "$cronjob" >> crontab_telit
-		crontab crontab_telit
+	if ! grep -q "$cronjob" crontab_current; then
+		printf '%s\n' "$cronjob" >> crontab_current
+		crontab crontab_current
 		printf '\n'
 	else
-		printf '%s\n' 'Cron job already exists'
+		printf '%s\n\n' 'Cron job already exists'
 	fi
-	rm crontab_telit
+	rm crontab_current
