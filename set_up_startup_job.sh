48874bf6c8fc5338dd1bf8ec30b31223654f20a9
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-21 16:38:49 +0100

    Improved existing cron job search string

--- set_up_startup_job.sh
+++ set_up_startup_job.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-	printf '%s\n\n' 'Setting up the cron job for script automation'
+	printf '%s\n' 'Setting up the cron job for script automation'
 	DIR="$(dirname "$(realpath "$0")")"
 	crontab -l > crontab_telit
-	if ! grep -q 'start_gnss_stream.py' crontab_telit; then
-		printf '%s\n' "@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1" >> crontab_telit
+	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1"
+	if ! grep -q $cronjob crontab_telit; then
+		printf '%s\n' $cronjob >> crontab_telit
 		crontab crontab_telit
+		printf '\n'
+	else
+		printf '%s\n' 'Cron job already exists'
 	fi
 	rm crontab_telit
