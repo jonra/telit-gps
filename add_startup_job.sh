ad72be8b17335d9c986ff5df305490c69336f452
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-22 03:42:26 +0100

    Final touch

--- add_startup_job.sh
+++ add_startup_job.sh
@@ -2,7 +2,7 @@
 
 	printf '%s\n' 'Adding the cron job for script automation'
 	DIR="$(dirname "$(realpath "$0")")"
-	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1"
+	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/gnss_to_mqtt.py >> ${HOME}/telit.log 2>&1"
 	crontab -l > crontab_current
 	if ! grep -q "$cronjob" crontab_current; then
 		printf '%s\n' "$cronjob" >> crontab_current
--- remove_startup_job.sh
+++ remove_startup_job.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-	printf '%s\n' 'Stopping GNSS_to_MQTT.py'
-	kill $(ps -ef | grep '[G]NSS_to_MQTT.py' | awk '{print $2}') 2> /dev/null
+	printf '%s\n' 'Stopping gnss_to_mqtt.py'
+	kill $(ps -ef | grep '[g]nss_to_mqtt.py' | awk '{print $2}') 2> /dev/null
 
 	printf '%s\n' 'Initiating the script that stops the GNSS stream'
 	./stop_gnss_stream.py >> "${HOME}/telit.log" 2>&1
 
 	printf '%s\n' 'Removing the cron job for script automation'
 	DIR="$(dirname "$(realpath "$0")")"
-	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/GNSS_to_MQTT.py >> ${HOME}/telit.log 2>&1"
+	cronjob="@reboot sleep 10 && ${DIR}/clear_log.sh && ${DIR}/start_gnss_stream.py >> ${HOME}/telit.log 2>&1 && ${DIR}/gnss_to_mqtt.py >> ${HOME}/telit.log 2>&1"
 	crontab -l > crontab_current
 	if grep -q "$cronjob" crontab_current; then
 		grep -v "$cronjob" crontab_current > tmpfile
