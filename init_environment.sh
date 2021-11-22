f64f48248739448c3b21ce43d597f996876b5d13
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-22 23:11:17 +0100

    Added screen

--- init_environment.sh
+++ init_environment.sh
@@ -2,7 +2,7 @@
 
 	printf '%s\n' 'Installing the required packages'
 	sudo apt update
-	sudo apt -y install minicom python3-pip libqmi-utils udhcpc
+	sudo apt -y install minicom python3-pip libqmi-utils udhcpc screen
 	
 	printf '%s\n' 'Installing the required Python3 libraries'
 	pip3 install pyserial paho-mqtt
