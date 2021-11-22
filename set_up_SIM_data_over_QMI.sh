ff7e3a6c7f520774585967423cb6379a6d1c9807
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-22 02:49:19 +0100

    Fixed

--- set_up_SIM_data_over_QMI.sh
+++ set_up_SIM_data_over_QMI.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 	printf '%s\n' 'Setting up the SIM data connection over QMI interface'
-	sleep 1
 	
 	printf '%s\n' 'Checking the compatibility of the module'
 	i=0
@@ -16,20 +15,16 @@
 		printf '%s\n\n' "Module doesn't support QMI interface, terminating"
 		exit 1
 	fi
-	sleep 1
 	
 	printf '%s\n' 'Making sure the module is ready'
 	if ! sudo qmicli -d /dev/cdc-wdm0 --dms-get-operating-mode | grep -q 'online'; then
 		sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'
 	fi
-	sleep 1
 	
 	printf '%s\n' 'Configuring the network interface for the raw-ip protocol'
 	sudo ip link set wwan0 down
-	sleep 1
 	printf '%c\n' 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip > /dev/null
 	sudo ip link set wwan0 up
-	sleep 1
 	
 	printf '%s\n' 'Connecting to the mobile network'
 	# Change the apn='...', or add username='...' and password='...' after apn='...' according to the information provided by your SIM operator
@@ -45,10 +40,7 @@
 		params="${params},password='${pw}'"
 	fi
 	sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="${params},ip-type=4" --client-no-release-cid
-	sleep 1
 	
 	printf '%s\n' 'Configuring the IP address and the default route'
 	sudo udhcpc -q -f -i wwan0
 	printf '\n'
-	
-	un=python3 -c "from json import load as json_load;with open('mqtt_params.json') as mqtt_json: print(json_load(mqtt_json)['username']"
