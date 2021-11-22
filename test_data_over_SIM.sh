573915c40fbd50bc1fbc53a8232d13c02effe340
Aleksandar Ritan <aleksandar.ritan@outlook.com>
2021-11-22 21:50:23 +0100

    Updated

--- test_data_over_SIM.sh
+++ test_data_over_SIM.sh
@@ -3,8 +3,12 @@
 	sudo ip link set eth0 down
 	sleep 10
 	
-	wget -q --spider http://google.com
-
-	if [ ! $? -eq 0 ]; then
-		sudo ip link set eth0 up
+	wget -q --spider https://www.google.com/
+	
+	if [ $? -eq 0 ]; then
+		printf '%s\n' 'Data over SIM is working'
+	else
+		printf '%s\n' 'Data over SIM is not working'
 	fi
+	
+	sudo ip link set eth0 up
